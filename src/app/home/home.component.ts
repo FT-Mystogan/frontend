0f79c8483eef5ad36b7d5de033b65d36ed5d726d
FT-Mystogan <viponlyc@gmail.com>
2023-02-22 14:21:10 +0700

    add service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HomeService } from '../Services/home.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  public key = 1;
+  public firstArray: any;
+  public lastArray: any;
+  public check = false;
+  constructor(private serive: HomeService) {
+    this.firstArray = [];
+    this.lastArray = [];
+  };
+  sort() {
+    this.serive.sort(this.key, this.firstArray).subscribe((data => {
+      this.lastArray = data;
+    }));
+  }
 
+  generate() {
+    this.serive.generate().subscribe((data => {
+      this.firstArray = data;
+    }));
+    this.check = true;
+  }
+
+  clear() {
+    this.lastArray = [];
+  }
 }
